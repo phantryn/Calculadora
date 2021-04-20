b561c25490b23c7b7d3dfdfaf3accd41ce323634
phantryn <60747457+phantryn@users.noreply.github.com>
2021-04-20 16:36:18 -0500

    Add files via upload

---
+++ Calculadora/operaciones basicas/src/operaciones_basica/Operaciones.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones_basica;
+
+/**
+ *
+ * @author Fernando Landeros
+ */
+public class Operaciones {
+    
+    public double result;
+   
+    public void calsuma(double n1, double n2){
+        result = n1 + n2;
+    }
+     public void calresta(double n1, double n2){
+        result = n1 - n2;
+}
+      public void calmulti(double n1, double n2){
+        result = n1 * n2;
+        
+      }
+       public void caldivi(double n1, double n2){
+        result = n1 / n2;
+        
+       }
+}
